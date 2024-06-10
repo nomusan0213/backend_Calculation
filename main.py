055d2e686d59dbb33b38aebd49df51b9c975ce44
nomusan0213 <yuta.inti.raymi0213@gmail.com>
2024-06-10 21:33:25 +0900

    [fix] CORS解決案

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://frontend-calculation-1.onrender.com/",
-    "*"
+    "https://frontend-calculation-1.onrender.com"
 ]
 
 app.add_middleware(
